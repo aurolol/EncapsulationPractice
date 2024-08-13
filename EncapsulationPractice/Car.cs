2f75395ba622d58dbe35a70cc8076f6b04c9ea10
auro <auro@DESKTOP-AV8I9BT>
2024-08-13 18:40:41 +0300

    Add project files.

---
+++ EncapsulationPractice/Car.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationPractice
+{
+    public class Car
+    {
+        private string _brand;
+        private string _model;
+        private string _clour;
+        private int _doorCount;
+
+
+        public string Brand { get { return _brand; } set { _brand = value; } }
+        public string Model { get { return _model; } set { _model = value; } }
+        public string Clour { get { return _clour; } set { _clour = value; } }
+        public int DoorCount
+        {
+            get { return _doorCount; }
+
+            set
+            {
+                if (value == 2 || value == 4)
+                {
+                    _doorCount = value;
+                }
+
+                else
+                {
+                    _doorCount = -1;
+                    Console.WriteLine("Kapı sayısı sadece 2 ve 4 olabilir.");
+                }
+            }
+        }
+
+        public Car()
+        {
+
+        }
+
+        public Car(string _brand, string _model, string _clour, int _doorCount)
+        {
+            Brand = _brand;
+            Model = _model;
+            Clour = _clour;
+            DoorCount = _doorCount;
+        }
+
+        public void CarResult()
+        {
+            Console.WriteLine($"Arabanın markası: {_brand}\nArabanın modeli: {_model}\nArabanın rengi: {_clour}\nArabanın Kapı Sayısı: {_doorCount}");
+        }
+
+
+    }
+}
---
+++ EncapsulationPractice/Program.cs
@@ -0,0 +1,13 @@
+﻿using EncapsulationPractice;
+
+Car car1 = new Car();
+car1.Brand = "Ford";
+car1.Model = "Focus";
+car1.Clour = "Mavi";
+car1.DoorCount = 2;
+car1.CarResult();
+
+Console.WriteLine("----------------------------------------");
+
+Car car2 = new Car("Ford", "Focus", "Blue", 3);
+car2.CarResult();
